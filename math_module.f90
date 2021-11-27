8da81b86a992c7a0dac104017f9157994ddb83a4
a0171610 <ozaki.m.mi.ys@gmail.com>
2021-11-27 13:45:40 +0900

    atanを追加

--- math_module.f90
+++ math_module.f90
@@ -8,4 +8,34 @@ module math_module
     math_pi2 = 2.0d0*math_pi, math_pih = 0.5d0*math_pi, &
     math_deg2rad = math_pi/180.0d0
 
+contains
+
+  function math_atan2(y,x) result(theta)
+    implicit none
+  
+    real(8), intent(in) :: x, y
+    real(8) :: theta
+  
+    if ((x /= 0.0d0) .and. (y /= 0.0d0)) then
+          theta = atan2(y,x)
+          if (theta < 0) then
+            theta = theta + math_pi * 2.0d0
+          end if
+    else if (x == 0.0d0) then
+        if (y > 0.0d0) then
+            theta = math_pi * 0.5d0
+        else if (y < 0.0d0) then
+            theta = math_pi + 0.5d0 * math_pi
+        else
+            theta = 0.0d0
+        end if
+    else if (y == 0.0d0) then
+        if (x >= 0.0d0) then
+            theta = 0.0d0
+        else
+            theta = math_pi
+        end if 
+    end if
+  
+  end function math_atan2
 end module math_module
