2f9af689c84f6cc79f280ab22b02f1707d2924a9
a0171610 <Ozaki.m.mi.ys@gami.com>
2021-12-22 14:28:46 +0900

    charの長さ設定を変更

--- time_module.f90
+++ time_module.f90
@@ -5,6 +5,6 @@ module time_module
   integer(8), public ::  nstep = 80, hstep = 5
   real(8), public :: deltat = 21600.0d0, kappa = 0.0d0
 
-  character(len=7), public :: &
+  character(len=10), public :: &
     model = "nisl", imethod = "bilin", imethoduv = "bilin"
 end module time_module
