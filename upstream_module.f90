ee4e04bb838985849bdab18f3f070959562002d7
a0171610 <Ozaki.m.mi.ys@gami.com>
2021-12-22 15:24:10 +0900

    速度を計算する関数を追加

--- upstream_module.f90
+++ upstream_module.f90
@@ -80,5 +80,49 @@ module upstream_module
       end do
       
     end subroutine find_points
+
+    subroutine calc_niuv(dt, p, q, lon, lat, gum, gvm)
+      use grid_module, only: latitudes => lat, longitudes => lon
+      use math_module, only: math_pi, pi2=>math_pi2
+      use time_module, only: imethoduv
+      use sphere_module, only: xyz2uv, lonlat2xyz
+      use uv_module, only: uv_sbody_calc
+      implicit none
+      real(8), intent(in) :: dt
+      integer(8), intent(in) :: p, q
+      real(8), intent(in) :: lon, lat
+      real(8), intent(out) :: gum, gvm
+      integer(8) :: nlon, nlat
+
+      real(8) :: xg, yg, zg, xr, yr, zr, xm, ym, zm, xdot, ydot, zdot, lon_grid, lat_grid, u, v, b
+      real(8) :: midlon, midlat
+
+
+      lon_grid = longitudes(p)
+      lat_grid = latitudes(q)
+      call lonlat2xyz(lon_grid, lat_grid, xr, yr, zr)
+      ! arrival points
+      call lonlat2xyz(lon, lat, xg, yg, zg)
+
+      b = 1.0d0 / sqrt( 2.0d0 * (1.0d0 + (xg*xr + yg*yr + zg*zr)) ) ! Ritchie1987 式(44)
+      xm = b * (xg + xr)
+      ym = b * (yg + yr)
+      zm = b * (zg + zr)
+      midlon = modulo(atan2(ym, xm) + pi2, pi2)
+      midlat = asin(zm)
+
+      xdot = (xg - xr) / dt
+      ydot = (yg - yr) / dt
+      zdot = (zg - zr) / dt
+      call xyz2uv(xdot, ydot, zdot, midlon, midlat, u, v)  !Richie1987式(49)
+      gum = gum + u
+      gvm = gvm + v
+
+      call uv_sbody_calc(midlon, midlat, u, v)
+      gum = gum - u
+      gvm = gvm - v
+
+
+    end subroutine calc_niuv
   
   end module upstream_module
